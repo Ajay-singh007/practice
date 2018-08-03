01ff768e914bd8fa5b76b8276264d959c190fb01
Josh <josh.wynn27@gmail.com>
2018-08-03 11:13:40 -0400

    JewelsAndStones
    -Rated: Easy
    -Solved in under 5 minutes. A simple problem

---
+++ JewelsAndStones/practice/practice/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string J = "z";
+            string S = "ZZ";
+            //Input: J = "aA", S = "aAAbbbb"
+            while (true)
+            {
+                Console.WriteLine(NumJewelsInStones(J, S));
+            }
+
+
+        }
+
+        private static int NumJewelsInStones(string J, string S)
+        {
+            int tmp = 0;
+            foreach (char c in S)
+            {
+                if (J.Contains(c))
+                {
+                    tmp++;
+                }
+            }
+
+            return tmp;
+        }
+    }
+
+
+}
